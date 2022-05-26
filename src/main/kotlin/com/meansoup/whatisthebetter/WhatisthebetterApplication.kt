3d5bf861af53f1627bcbc3084c48fbdb86daa164
meansoup <alsrnr1210@gmail.com>
2022-05-27 00:59:33 +0900

    first commit

---
+++ src/main/kotlin/com/meansoup/whatisthebetter/WhatisthebetterApplication.kt
@@ -0,0 +1,11 @@
+package com.meansoup.whatisthebetter
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WhatisthebetterApplication
+
+fun main(args: Array<String>) {
+	runApplication<WhatisthebetterApplication>(*args)
+}
---
+++ src/test/kotlin/com/meansoup/whatisthebetter/WhatisthebetterApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.meansoup.whatisthebetter
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WhatisthebetterApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
