b2d82c079efd61e3f4d78d87e76e36b8f1901f69
meansoup <alsrnr1210@gmail.com>
2022-05-31 01:39:12 +0900

    feat: DomainException과 하위 Exception 추가

---
+++ src/main/kotlin/com/meansoup/whatisthebetter/domain/exception/DomainException.kt
@@ -0,0 +1,7 @@
+package com.meansoup.whatisthebetter.domain.exception
+
+open class DomainException(message: String): RuntimeException(message)
+
+class TooLongUsernameException(message: String): DomainException(message)
+class TooSmallUsernameException(message: String): DomainException(message)
+class InvalidCharacterInUsernameException(message: String): DomainException(message)
