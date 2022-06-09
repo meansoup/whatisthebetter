8f8eaed4a2e18c09c15df8f2feb5ffc30a3f814c
meansoup <alsrnr1210@gmail.com>
2022-06-10 00:49:55 +0900

    feat: Post에 owner 추가

--- src/main/kotlin/com/meansoup/whatisthebetter/domain/post/Post.kt
+++ src/main/kotlin/com/meansoup/whatisthebetter/domain/post/Post.kt
@@ -1,15 +1,19 @@
 package com.meansoup.whatisthebetter.domain.post
 
+import com.meansoup.whatisthebetter.domain.user.User
+
 class Post {
     val id: String
+    var owner: User
     var title: Title
     var content1: Content
     var content2: Content
     var createdAt: Long
     var modifiedAt: Long
 
-    constructor(id: String, title: Title, content1: Content, content2: Content) {
+    constructor(id: String, owner: User, title: Title, content1: Content, content2: Content) {
         this.id = id
+        this.owner = owner
         this.title = title
         this.content1 = content1
         this.content2 = content2
