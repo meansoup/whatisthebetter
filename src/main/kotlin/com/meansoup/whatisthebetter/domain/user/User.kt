9b9ee65b24871254981862516ffc46cd0dcc60e0
meansoup <alsrnr1210@gmail.com>
2022-06-13 01:18:22 +0900

    feat: User에 UUID type의 id 추가

--- src/main/kotlin/com/meansoup/whatisthebetter/domain/user/User.kt
+++ src/main/kotlin/com/meansoup/whatisthebetter/domain/user/User.kt
@@ -1,11 +1,15 @@
 package com.meansoup.whatisthebetter.domain.user
 
+import java.util.*
+
 class User {
+    val id: UUID
     val name: Username
     val email: Email
     val createdAt: Long
 
     constructor(name: Username, email: Email) {
+        this.id = UUID.randomUUID()
         this.name = name
         this.email = email
 
