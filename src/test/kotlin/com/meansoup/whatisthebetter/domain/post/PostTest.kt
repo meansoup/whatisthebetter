8da512f7c10892bd09ce8dd174e5d1d6599bfea3
meansoup <alsrnr1210@gmail.com>
2022-06-13 21:00:13 +0900

    feat: Post의 id에 UUID 적용

--- src/test/kotlin/com/meansoup/whatisthebetter/domain/post/PostTest.kt
+++ src/test/kotlin/com/meansoup/whatisthebetter/domain/post/PostTest.kt
@@ -1,7 +1,6 @@
 package com.meansoup.whatisthebetter.domain.post
 
 import com.meansoup.whatisthebetter.domain.user.UserMother
-import org.apache.commons.lang3.RandomStringUtils
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.data.Offset
 import org.junit.jupiter.api.Test
@@ -9,19 +8,18 @@ import org.junit.jupiter.api.Test
 internal class PostTest {
 
     @Test
-    fun `생성 시에 현재시각의 createdAt, modifiedAt이 설정된다`() {
+    fun `생성 시에 현재시각의 createdAt, modifiedAt이 설정되고 id가 부여된다`() {
         // given
-        val id = RandomStringUtils.randomAlphanumeric(10)
         val owner = UserMother.generate()
         val title = TitleMother.generate()
         val target1 = ContentMother.generate()
         val target2 = ContentMother.generate()
 
         // when
-        val post = Post(id, owner, title, target1, target2)
+        val post = Post(owner, title, target1, target2)
 
         // then
-        assertThat(post.id).isEqualTo(id)
+        assertThat(post.id).isNotNull()
         assertThat(post.owner).isEqualTo(owner)
         assertThat(post.title).isEqualTo(title)
         assertThat(post.content1).isEqualTo(target1)
