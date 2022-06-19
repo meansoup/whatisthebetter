5fdfbf527b9fede2db60a705481b045efd8d4776
meansoup <alsrnr1210@gmail.com>
2022-06-19 19:37:48 +0900

    feat: PostService에 getPost 추가

--- core/src/test/kotlin/com/meansoup/whatisthebetter/domain/post/PostServiceTest.kt
+++ core/src/test/kotlin/com/meansoup/whatisthebetter/domain/post/PostServiceTest.kt
@@ -1,12 +1,12 @@
 package com.meansoup.whatisthebetter.domain.post
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.ArgumentMatchers.eq
 import org.mockito.InjectMocks
 import org.mockito.Mock
-import org.mockito.Mockito.times
-import org.mockito.Mockito.verify
+import org.mockito.Mockito.*
 import org.mockito.junit.jupiter.MockitoExtension
 
 @ExtendWith(MockitoExtension::class)
@@ -28,6 +28,21 @@ internal class PostServiceTest {
 
         // then
         verify(postRepository, times(1)).save(safeEq(post))
+    }
+
+    @Test
+    fun `get Post from repository by id`() {
+        // given
+        val post = PostMother.generate()
+        val id = post.id
+
+        doReturn(post).`when`(postRepository).findBy(safeEq(id))
+
+        // when
+        val found = sut.getPost(id)
+
+        // then
+        assertThat(found).isEqualTo(post)
     }
 
     fun <T : Any> safeEq(value: T): T = eq(value) ?: value
