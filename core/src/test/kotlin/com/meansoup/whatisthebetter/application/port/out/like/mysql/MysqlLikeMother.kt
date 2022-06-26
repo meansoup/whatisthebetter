7e3f5d4cdc374c17c22503da78c4fcf22a88f8a1
meansoup <alsrnr1210@gmail.com>
2022-06-26 20:08:53 +0900

    chore: MysqlLikeMother에서 사용하지 않는 변수 삭제 / factory 사용

--- core/src/test/kotlin/com/meansoup/whatisthebetter/application/port/out/like/mysql/MysqlLikeMother.kt
+++ core/src/test/kotlin/com/meansoup/whatisthebetter/application/port/out/like/mysql/MysqlLikeMother.kt
@@ -1,25 +1,12 @@
 package com.meansoup.whatisthebetter.application.port.out.like.mysql
 
 import com.meansoup.whatisthebetter.domain.like.LikeMother
-import com.meansoup.whatisthebetter.domain.post.Post
-import org.jeasy.random.EasyRandom
-import org.jeasy.random.EasyRandomParameters
 
 class MysqlLikeMother {
     companion object {
-        private val parameters = EasyRandomParameters().seed(System.currentTimeMillis())
-        private val easyRandom: EasyRandom = EasyRandom(parameters)
-
         fun generatePost(): MysqlLike {
             val likePost = LikeMother.generatePost()
-            val post = likePost.thing
-
-            val likedId = when (post) {
-                is Post -> post.id.toString()
-                else -> ""
-            }
-
-            return MysqlLike(likePost.user.id.toString(), likedId, likePost.createdAt)
+            return MysqlLikeFactory.create(likePost)
         }
 
         fun generatePost(likedId: String): MysqlLike {
