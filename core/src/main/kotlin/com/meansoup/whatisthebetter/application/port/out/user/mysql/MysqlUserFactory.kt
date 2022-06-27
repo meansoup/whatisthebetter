dcbf600eda1d36c8a343ab326c05f81aa3743faa
meansoup <alsrnr1210@gmail.com>
2022-06-27 22:45:37 +0900

    feat: MysqlUserFactory에서 mysqlUser -> user 변환 로직 추가

--- core/src/main/kotlin/com/meansoup/whatisthebetter/application/port/out/user/mysql/MysqlUserFactory.kt
+++ core/src/main/kotlin/com/meansoup/whatisthebetter/application/port/out/user/mysql/MysqlUserFactory.kt
@@ -1,6 +1,10 @@
 package com.meansoup.whatisthebetter.application.port.out.user.mysql
 
+import com.meansoup.whatisthebetter.domain.exception.NullPropertyException
+import com.meansoup.whatisthebetter.domain.user.Email
 import com.meansoup.whatisthebetter.domain.user.User
+import com.meansoup.whatisthebetter.domain.user.Username
+import java.util.*
 
 class MysqlUserFactory {
 
@@ -9,8 +13,21 @@ class MysqlUserFactory {
             return MysqlUser(
                 user.id.toString(),
                 user.name.name,
-                user.email.toString(),
+                user.email.name,
                 user.createdAt
+            )
+        }
+
+        fun create(mysqlUser: MysqlUser): User {
+            val name = mysqlUser.name ?: throw NullPropertyException("name is null in: " + mysqlUser.id)
+            val email = mysqlUser.email ?: throw NullPropertyException("email is null in: " + mysqlUser.id)
+            val createdAt = mysqlUser.createdAt ?: throw NullPropertyException("createdAt is null in: " + mysqlUser.id)
+
+            return User(
+                UUID.fromString(mysqlUser.id),
+                Username(name),
+                Email(email),
+                createdAt
             )
         }
     }
